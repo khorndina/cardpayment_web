0a7b62f13c147dab8db04497b0b62b8483aa05c7
khorn <khorndinaa@gmail.com>
2023-08-07 17:25:58 +0700

    update file

--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\backend\AdminController;
+use App\Http\Controllers\backend\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 // admin route
 Route::get('dashboard',[AdminController::class, 'dashboard'])->name('dashboard');
 
-// when user submit form login
-// Route::post('dashboard',[AdminController::class, 'dashboard'])->name('login');
+// update profile route
+Route::get('profile',[ProfileController::class, 'index'])->name('profile');
